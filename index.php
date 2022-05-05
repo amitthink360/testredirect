4e345ec6c8103298eb3e689ed6f6aac5e9336a88
amitthink360 <amit.think360@gmail.com>
2022-05-06 00:05:03 +0530

    Update code

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-echo"<pre/>";print_r($_GET);die;
-/* class Database
+class Database
 {
     private static $dbName = 'domain_system';
     private static $dbHost = '147.182.189.109';
@@ -34,27 +33,23 @@ echo"<pre/>";print_r($_GET);die;
 }
 
 $pdo = Database::connect();
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-/* $current = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$variables = explode("/",$current);
-echo"<pre/>";print_r($variables);die; */
-/* try {
+try {
 	$sql = "INSERT INTO clicker_phone (phone,date_added) values (?,?)";
 	$q = $pdo->prepare($sql);
-	$q->execute(array($variables[1],date('Y-m-d H:i:s')));
+	$q->execute(array($_GET['phone'],date('Y-m-d H:i:s')));
 }catch(Exception $e) {
 	//echo 'Message: ' .$e->getMessage();
 }
-
-$parms = array_filter($variables, fn($value) => !is_null($value) && $value !== '');
-$queryString = implode("&",$parms);
-if($variables[3] == 'za'){
+echo"<pre/>";print_r($_GET);die;
+$queryString =  http_build_query($_GET);
+if($_GET['dig'] == 'za'){
 	$redirect_link = "https://go.myquickresource.com/4d2d4d65-8271-4f15-9046-0be935eeaa74?optin_domain=050522_055_com&gender=050522_com&filename=POPEYE_050522_com&email=POPEYE_050522_&".$queryString;
-}elseif($variables[3] == 'ya'){
+}elseif($_GET['dig'] == 'ya'){
 	$redirect_link = "https://go.myquickresource.com/8816826c-618f-45be-b429-68a19646f7d3?optin_domain=050522_055_com&gender=050522_com&filename=MCD_050522_com&email=MCD_050522_&".$queryString;
 }
 
-header("Location:".$redirect_link); */
+header("Location:".$redirect_link);
 
 ?>
